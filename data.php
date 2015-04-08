f9d5c36d623e64a359e097ee731efd708042c5fc
Gene Kobilansky <genek149@gmail.com>
2015-04-08 13:39:01 -0400

    Update data.php

--- data.php
+++ data.php
@@ -1,6 +1,6 @@
 
 /* gets url */
-function get_content_from_github($url) {
+function get_forks_from_github($url) {
 	$ch = curl_init();
 	curl_setopt($ch,CURLOPT_URL,$url);
 	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1); 
